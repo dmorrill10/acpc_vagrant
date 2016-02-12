0e2e8bd71d308898d6a2fa5316113a29517da3d2
Dustin Morrill <morrill@ualberta.ca>
2016-02-11 17:54:21 -0700

    Add pypy-4.01 (python2) since it's faster than pypy3

--- my_cookbooks/python/recipes/default.rb
+++ my_cookbooks/python/recipes/default.rb
@@ -33,25 +33,27 @@ execute "Install Anaconda" do
   not_if { File.exists?(install) }
 end
 
-pypy_version = 'pypy3-2.4.0-linux64'
-pypy_url = "https://bitbucket.org/pypy/pypy/downloads/#{pypy_version}.tar.bz2"
-pypy_compressed = pypy_url.split('/').last
+pypy_versions = {'pypy' => 'pypy-4.0.1-linux64', 'pypy3' => 'pypy3-2.4.0-linux64'}
+pypy_versions.each do |name, version|
+  pypy_url = "https://bitbucket.org/pypy/pypy/downloads/#{version}.tar.bz2"
+  pypy_compressed = pypy_url.split('/').last
 
-pypy_install_location = File.join(Dir.home(user), '.pypy')
+  pypy_install_location = File.join(Dir.home(user), ".#{name}")
 
-remote_file "Download pypy" do
-  path File.join(Chef::Config[:file_cache_path], pypy_compressed)
-  source pypy_url
-  mode '777'
-  user user
-  group user
-  not_if { File.exists?(pypy_install_location) }
-end
+  remote_file "Download #{name}" do
+    path File.join(Chef::Config[:file_cache_path], pypy_compressed)
+    source pypy_url
+    mode '777'
+    user user
+    group user
+    not_if { File.exists?(pypy_install_location) }
+  end
 
-execute "Extract #{pypy_compressed}" do
-  cwd Chef::Config[:file_cache_path]
-  user user
-  group user
-  command "tar -jxf #{pypy_compressed} && mv #{pypy_version} #{pypy_install_location}"
-  not_if { File.exists?(pypy_install_location) }
+  execute "Extract #{pypy_compressed}" do
+    cwd Chef::Config[:file_cache_path]
+    user user
+    group user
+    command "tar -jxf #{pypy_compressed} && mv #{version} #{pypy_install_location}"
+    not_if { File.exists?(pypy_install_location) }
+  end
 end
